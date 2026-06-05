52abe04faed25f1c3bdbe01950caed2ae4009de2
Matthew Swanberg <matt20swanberg@gmail.com>
2026-06-04 21:38:31 -0400

    Implement toy fetching and creation

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,37 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import Header from "./Header";
 import ToyForm from "./ToyForm";
 import ToyContainer from "./ToyContainer";
 
 function App() {
+
+  const [toys, setToys] = useState([])
+
   const [showForm, setShowForm] = useState(false);
 
   function handleClick() {
     setShowForm((showForm) => !showForm);
   }
 
+  useEffect(() => {
+    fetch("http://localhost:3001/toys")
+      .then((res) => res.json())
+      .then((toyData) => setToys(toyData));
+  }, []);
+
+  function addToy(newToy) {
+    setToys((currentToys) => [...currentToys, newToy])
+  }
+
   return (
     <>
       <Header />
-      {showForm ? <ToyForm /> : null}
+      {showForm ? <ToyForm addToy={addToy} /> : null}
       <div className="buttonContainer">
         <button onClick={handleClick}>Add a Toy</button>
       </div>
-      <ToyContainer />
+      <ToyContainer toys={toys} />
     </>
   );
 }
